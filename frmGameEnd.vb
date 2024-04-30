d8d504dd1fe7b5e8915e72cdf2be9184ba365ead
Callum <callumtbennett@gmail.com>
2024-04-30 12:48:51 +1000

    Update frmGameEnd.vb
    Logbook update with previous commits
    
    In frmGameEnd
    
    Changed reading all scores to reading top 10. 
    
    Also changed file format for hs.txt from 
    name|score
    name|score to
    name 
    name
    score
    score
    
    Done mainline, import scores > add current score > sort scores > write scores
    
    Remaining for this form, Internal documentation.

--- frmGameEnd.vb
+++ frmGameEnd.vb
@@ -7,7 +7,13 @@ Public Class frmGameEnd
 
     Private Sub frmGameEnd_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         lblScore.Text = "Score: " & playerScore
+        readHighScores()
+        arrHighScores(11).name = playerName
+        arrHighScores(11).score = playerScore
+        BubbleSort()
+        writeScore()
     End Sub
+    
     Public Structure recHighScore
         Public name As String
         Public score As Integer
@@ -35,38 +41,49 @@ Public Class frmGameEnd
             Last = Last - 1
         End While
     End Sub
+    
     Private Sub printHighScores()
-        For u = 1 To 5
-            lstTopScores.Items.Add(u & " " & arrHighScores(u).name & " " & arrHighScores(u).score)
+        For u = 1 To 10
+            lstTopScores.Items.Add(u & ": " & arrHighScores(u).name & ", " & arrHighScores(u).score)
         Next u
     End Sub
+    
     Private Sub readHighScores()
         Dim temp As String = ""
-        Dim temp2 As Integer = 0
 
         FileSystem.FileOpen(1, "./hs.txt", OpenMode.Input)
 
-
-        While Not EOF(1)
+        For i = 1 to 10
             FileSystem.Input(1, temp)
-            arrHighScores(nextInt).name = temp
-            FileSystem.Input(1, temp2)
-            arrHighScores(nextInt).score = temp2
-            nextInt = nextInt + 1
-        End While
+            arrHighScores(i).name = temp
+        Next i
+
+        For i = 1 to 10
+            FileSystem.Input(1, temp)
+            arrHighScores(i).score = CInt(temp2)
+        Next i
 
         FileSystem.FileClose(1)
     End Sub
-    Private Sub Swap(ByRef A As recHighScore, ByRef B As recHighScore)
+
+    Private Sub Swap(ByVal A As recHighScore, ByVal B As recHighScore)
         Dim Temp As recHighScore
         Temp = A
         A = B
         B = Temp
     End Sub
+
     Private Sub writeScore()
-        FileSystem.FileOpen(1, "hs.txt", OpenMode.Append)
-        FileSystem.WriteLine(1, playerName)
-        FileSystem.WriteLine(1, playerScore)
+        FileSystem.FileOpen(1, "hs.txt", OpenMode.Write)
+
+        For i = 1 to 10 
+            FileSystem.WriteLine(1, arrHighScores(i).name
+        Next i
+
+        For i = 1 to 10
+            FileSystem.WriteLine(1, arrHighScores(i).score
+        Next i
+
         FileSystem.FileClose(1)
     End Sub
 
