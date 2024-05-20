efea28b5ff865075b3c703848d2a39afeda5b968
Callum <callumtbennett@gmail.com>
2024-05-20 11:50:25 +1000

    20/05/24
    Tutorial PPTX

--- Game Files/frmStart.Designer.vb
+++ Game Files/frmStart.Designer.vb
@@ -33,7 +33,6 @@ Partial Class frmStart
         rdoComputerUnfair = New RadioButton()
         rdoComputerHard = New RadioButton()
         rdoComputerMedium = New RadioButton()
-        Button1 = New Button()
         grpComputerLevel.SuspendLayout()
         SuspendLayout()
         ' 
@@ -129,15 +128,6 @@ Partial Class frmStart
         rdoComputerMedium.Text = "Medium"
         rdoComputerMedium.UseVisualStyleBackColor = True
         ' 
-        ' Button1
-        ' 
-        Button1.Location = New Point(668, 58)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(75, 36)
-        Button1.TabIndex = 11
-        Button1.Text = "Button1"
-        Button1.UseVisualStyleBackColor = True
-        ' 
         ' frmStart
         ' 
         AcceptButton = btnStart
@@ -148,7 +138,6 @@ Partial Class frmStart
         BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
         BackgroundImageLayout = ImageLayout.Zoom
         ClientSize = New Size(827, 590)
-        Controls.Add(Button1)
         Controls.Add(grpComputerLevel)
         Controls.Add(txtPlayerName)
         Controls.Add(btnStart)
@@ -175,6 +164,5 @@ Partial Class frmStart
     Friend WithEvents rdoComputerUnfair As RadioButton
     Friend WithEvents rdoComputerHard As RadioButton
     Friend WithEvents rdoComputerMedium As RadioButton
-    Friend WithEvents Button1 As Button
 
 End Class
